95999dec76268e370701c227a95b08b2d03d81bf
shagor900 <shagor900@gmail.com>
2023-01-04 19:17:16 -0500

    Added a new project

---
+++ src/basicprictice/Calculator.java
@@ -0,0 +1,66 @@
+package basicprictice;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+
+		/*String name;
+
+		name = "Divisioin";
+
+		int x = 10;
+		int y = 5;
+
+		int div;
+
+		div = x / y;
+
+		System.out.println(name + "  :  " + div);*/
+		
+		/*String name;
+		
+		name = "AddingValue";
+				
+		int x = 10;
+		int y = 20;
+
+		int add;
+		
+		add = x+y;
+		
+		System.out.println(name+"  : "+add);*/
+		
+	  /*String name;
+		
+	  name = "subtraction";
+			  
+      int x = 50;
+	  int y = 30;
+	  
+	  int subtraction;
+	  
+	  subtraction = x-y;
+	  
+	  System.out.println(name+"   : "+subtraction);*/
+		
+		String name;
+		name = "Multification" ;
+		
+		int x = 100;
+		int y = 50;
+		
+		int multiply;
+		
+		multiply = x*y;
+		
+		System.out.println(name+"  :  "+multiply);
+		
+		
+		
+	  
+	  
+	  
+	  
+	}
+
+}
